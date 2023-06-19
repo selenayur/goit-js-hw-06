39a1e9b8cc21526ba2d4ccf5b74199acf46793eb
selenayur <125310718+selenayur@users.noreply.github.com>
2023-06-19 21:09:35 +0300

    19062023

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,10 @@
 
 const itemAll = document.querySelectorAll('.item');
+
 console.log("Number of categories:", itemAll.length);
 
+
 itemAll.forEach(function(item) {
-    console.log("Category:", item.children[0].textContent),
-    console.log("Elements:", item.children[1].children.length);
+    console.log("Category:", item.firstElementChild.textContent),
+    console.log("Elements:", item.lastElementChild.children.length);
 });
--- js/task-05.js
+++ js/task-05.js
@@ -1,13 +1,14 @@
 //поиск селекторов
+
 const inputElem = document.querySelector('#name-input');
 const outputElem = document.querySelector('#name-output');
 
 
 const onOutputElem = event =>
-  { if (event.currentTarget.value !== '') {
-    outputElem.textContent = event.currentTarget.value;;
+    {if (event.currentTarget.value !== '') {
+    outputElem.textContent = event.currentTarget.value;
   } else {
-    onOutputElem.textContent = 'Anonymous';
+    outputElem.textContent = 'Anonymous';
   }
   }
 
